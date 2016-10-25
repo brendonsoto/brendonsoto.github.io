43e64f9847722772ad5a2ee7ed11886a4f4b57e1
brendon <brendon.soto@gmail.com>
2016-10-25 00:08:07 -0400

    Cleanup and added a little extra javascript to give the site's functionality some zing.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,16 @@
-$('body').on('click', '.nav a', function() {
+$('body').on('click', '.nav a', function(e) {
+  e.preventDefault();
+
+  // Switch active classes
   $('.nav .active').removeClass('active');
   $(this).parents('li').addClass('active');
+
+  // Scroll smoothly to the section
+  var idToScrollTo = this.href.substring(this.href.lastIndexOf('#'));
+  $('html, body').animate({
+    scrollTop: $(idToScrollTo).offset().top
+  }, 500);
+
+  // Hide nav menu on mobile
+  $('#navbar').collapse('hide');
 });
