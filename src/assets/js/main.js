aecde45992dd4ab95e698c9386bd1d8999d2a113
brendon <brendon.soto@gmail.com>
2016-11-19 16:48:14 -0500

    Revert "Cleanup and added a little extra javascript to give the site's functionality some zing."
    This reverts commit 8611b727ef98ceb4eda406104f7ae105587cbe7c.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,16 +1,4 @@
-$('body').on('click', '.nav a', function(e) {
-  e.preventDefault();
-
-  // Switch active classes
+$('body').on('click', '.nav a', function() {
   $('.nav .active').removeClass('active');
   $(this).parents('li').addClass('active');
-
-  // Scroll smoothly to the section
-  var idToScrollTo = this.href.substring(this.href.lastIndexOf('#'));
-  $('html, body').animate({
-    scrollTop: $(idToScrollTo).offset().top
-  }, 500);
-
-  // Hide nav menu on mobile
-  $('#navbar').collapse('hide');
 });
